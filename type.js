92a7882f5aa6cf3983bd803ef75b5f1a968fa191
parishah517 <80281830+parishah517@users.noreply.github.com>
2022-06-23 23:04:50 -0400

    website files upload

---
+++ type.js
@@ -0,0 +1,90 @@
+//source: https://github.com/tameemsafi/typewriterjs
+//author: Tameem Safi
+
+var t1 = document.getElementById('t1');
+
+var typewriter = new Typewriter(t1, {
+  loop: true,
+  delay: 150,
+});
+
+typewriter
+    
+    //.changeDelay(150)
+    .typeString('travel')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('doodle')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('watch Criminal Minds')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('go to the gym')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('listen to The Weeknd')
+    .deleteAll()
+    .start()
+
+var t2 = document.getElementById('t2');
+
+var typewriter = new Typewriter(t2, {
+  loop: true,
+  delay: 150,
+});
+
+typewriter
+    //.changeDelay(150)
+    .typeString('C++')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('Java')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('Python')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('MATLAB')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('R')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('HTML')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('CSS')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('React JS')
+    .deleteAll()
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('Django')
+    .start()
+
+var t3 = document.getElementById('t3');
+
+var typewriter = new Typewriter(t3, {
+  loop: true,
+  delay: 150,
+});
+
+typewriter
+    //.changeDelay(150)
+    .typeString('software engineering')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('app development')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('machine learning')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('web design')
+    .deleteAll()
+    .pauseFor(1000)
+    .typeString('full-stack development')
+    .deleteAll()
+    .start()
